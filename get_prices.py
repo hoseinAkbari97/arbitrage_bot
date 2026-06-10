1d56fb9214188fc5f253d6b138439a509104ab19
hoseinAkbari97 <hossein16akbari@gmail.com>
2026-06-10 14:53:48 +0330

    running on ubuntu-2

--- get_prices.py
+++ get_prices.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import requests
+
+import requests
+
+url = 'https://apiv2.nobitex.ir/v3/orderbook/USDTIRT'
+
+try:
+    # sending GET request
+    response = requests.get(url)
+    
+    # Check if the request was successfull
+    response.raise_for_status()
+    
+    # Formatting the answer
+    data = response.json()
+    
+    print(data)
+
+except requests.exceptions.RequestException as e:
+    print(f"خطایی رخ داده است: {e}")
